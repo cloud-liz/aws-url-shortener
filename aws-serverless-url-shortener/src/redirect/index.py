769b2bac42bb9c8f0b955adbfa3a23399f653a79
Liz Martin <gloremartin@gmail.com>
2025-12-03 16:00:41 +0300

    Update index.py
    Minimal Lambda function to redirect

--- aws-serverless-url-shortener/src/redirect/index.py
+++ aws-serverless-url-shortener/src/redirect/index.py
@@ -1,1 +1,28 @@
-2
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
+
+def lambda_handler(event, context):
+    short_id = event["pathParameters"]["shortId"]
+
+    response = table.get_item(Key={"shortId": short_id})
+    item = response.get("Item")
+
+    if not item:
+        return {"statusCode": 404, "body": "Not Found"}
+
+    long_url = item["longUrl"]
+
+    table.update_item(
+        Key={"shortId": short_id},
+        UpdateExpression="SET clicks = clicks + :val",
+        ExpressionAttributeValues={":val": 1}
+    )
+
+    return {
+        "statusCode": 301,
+        "headers": {"Location": long_url}
+    }
+
